b66d287abb14c3fad07825cd850c6ad704c0bfe9
Kreyo <matthew.timoshenko@gmail.com>
2013-07-02 10:30:32 +0300

    first steps
    symfony 2.3.1 with required vendors installed
    1st page w/ controller created (no custom css yet!)
    route planning in process

---
+++ src/Swed/JBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Swed\JBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SwedJBundle:Bank:index.html.twig');
+    }
+}
---
+++ src/Swed/JBundle/SwedJBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Swed\JBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SwedJBundle extends Bundle
+{
+}
